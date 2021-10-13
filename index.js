24a50ea90cd1e3b5aeb4c86636a90a357dc489ff
ShawnTownsend <stitans28@gmail.com>
2021-10-13 14:21:48 +0000

    Changes

--- index.js
+++ index.js
@@ -27,19 +27,17 @@ function oddAndEvenInFifteen(x){
 
 //3
 function assignGrade(score){
-    if (score > 90);
+    if (score < 90);
         return 'A'
-    else if (score > 80);
+    else if (score < 80);
         return 'B'
-    else if (score > 70);
+    else if (score < 70);
         return 'C'
-    else if (score > 64);
+    else if (score < 64);
         return 'D'
     else (score >= 64);
         return 'F'
 }
-
-assignGrade(72));
 
 
 
