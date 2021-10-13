3037d7a43e51a19ced71fd13e81f43ccbf67c8cc
ShawnTownsend <stitans28@gmail.com>
2021-10-13 14:15:08 +0000

    Completed questions 1-3

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 //1
 function sumOfFourAndSix(){
-  
+    let sum = 0
+    for (let i = 1, i < 1000, i++){
+        if (i % 4 === 0 && i % 6 === 0) {
+        sum += i;
+    }
+}  
+    return sum
+}
 }
 
 //2
-function oddAndEvenInFifteen(){
-  
+function oddAndEvenInFifteen(x){
+    for (let x = 0; x <= 15; x++) {
+        if (x === 0) {
+            console.log(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+            console.log(x + " is even");   
+        }
+        else {
+            console.log(x + " is odd");
+        }
+}
 }
 
 //3
-function assignGrade(){
-  
+function assignGrade(score){
+    if (score > 90);
+        return 'A'
+    else if (score > 80);
+        return 'B'
+    else if (score > 70);
+        return 'C'
+    else if (score > 64);
+        return 'D'
+    else (score >= 64);
+        return 'F'
 }
+
+assignGrade(72));
 
 
 
