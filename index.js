8d4a1872c5931fe7069811815916718eeb81d631
ShawnTownsend <stitans28@gmail.com>
2021-10-13 14:58:15 +0000

    More changes

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
 //1
 function sumOfFourAndSix(){
-    let sum = 0
-    for (let i = 1, i < 1000, i++){
+    let sum = 0;
+    for (let i = 0, i <= 1000, i++){
         if (i % 4 === 0 && i % 6 === 0) {
         sum += i;
     }
 }  
     return sum
 }
-}
 
 //2
-function oddAndEvenInFifteen(x){
-    for (let x = 0; x <= 15; x++) {
-        if (x === 0) {
-            console.log(x +  " is even");
-        }
-        else if (x % 2 === 0) {
-            console.log(x + " is even");   
+function oddAndEvenInFifteen(){
+    for (let i = 0; i < 16; i++) {
+        if (i % 2 === 0) {
+            console.log(i +  " is even");
         }
         else {
-            console.log(x + " is odd");
+            console.log(i + " is odd");   
         }
 }
 }
 
 //3
 function assignGrade(score){
-    if (score < 90);
-        return 'A'
-    else if (score < 80);
-        return 'B'
-    else if (score < 70);
-        return 'C'
-    else if (score < 64);
-        return 'D'
-    else (score >= 64);
-        return 'F'
+        if (score < 90) {
+            return 'A'
+        else if (score < 80){
+            return 'B'
+        else if (score < 70){
+            return 'C'
+        else if (score >= 65){
+            return 'D'
+        else {
+            return 'F'
 }
+    console.log(assignGrade(82));
 
 
 
